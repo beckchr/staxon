7308910784b3b0456d0d68c794e7ee522ed28bbc
Christoph Beck <beck@odysseus.de>
2011-10-21 20:55:46 +0200

    minor javadoc change

--- src/main/java/de/odysseus/staxon/xml/package-info.java
+++ src/main/java/de/odysseus/staxon/xml/package-info.java
@@ -1,9 +1,4 @@
 /**
- * <p>This package provides simple XML stream reader/writer classes.</p>
- * 
- * These classes are provided for demonstration purposes. Though functional,
- * they are not meant to compete with full flavored StAX implementations.
- * However, in an environment where no default StAX implementation is
- * available (e.g. mobile), it might be handy to use this implementation.
+ * <p>This package provides XML utility classes.</p>
  */
 package de.odysseus.staxon.xml;
