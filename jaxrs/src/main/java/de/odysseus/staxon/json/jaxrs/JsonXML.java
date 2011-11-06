6a497224dee1d1237c986a5172d72d12397194c3
Christoph Beck <beck@odysseus.de>
2011-11-06 21:30:24 +0100

    added javadoc

--- jaxrs/src/main/java/de/odysseus/staxon/json/jaxrs/JsonXML.java
+++ jaxrs/src/main/java/de/odysseus/staxon/json/jaxrs/JsonXML.java
@@ -25,10 +25,57 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.TYPE, ElementType.PARAMETER })
 public @interface JsonXML {
+	/**
+	 * <p>JSON documents may have have multiple root properties. However,
+	 * XML requires a single root element. This property takes the name
+	 * of a "virtual" root element, which will be removed from the stream
+	 * when writing and added to the stream when reading.</p>
+	 * 
+	 * <p>The default value is <code>""</code> (i.e. no virtual root).</p>
+	 */
 	String virtualRoot() default "";
+	
+	/**
+	 * <p>Specify array paths. paths are absolute with at least two names,
+	 * where names are separated by `'/'` and may be prefixed.</p>
+	 * <p>E.g. for</p>
+	 * <pre>
+	 * {
+	 *   "alice" : {
+	 *     "bob" : [ "edgar", "charlie" ],
+	 *     "peter" : null
+	 *   }
+	 * }
+	 * </pre>
+	 * <p>we would specify <code>"/alice/bob"</code> as multiple path.</p>
+	 */
 	String[] multiplePaths() default {};
+
+	/**
+	 * <p>Format output for better readability?</p>
+	 * 
+	 * <p>The default value is <code>false</code>.</p>
+	 */
 	boolean prettyPrint()  default false;
+
+	/**
+	 * <p>Trigger arrays automatically?</p>
+	 * 
+	 * <p>The default value is <code>false</code>.</p>
+	 */
 	boolean autoArray() default false;
+
+	/**
+	 * <p>Whether to write namespace declarations.</p>
+	 * 
+	 * <p>The default value is <code>true</code>.</p>
+	 */
 	boolean namespaceDeclarations() default true;
+
+	/**
+	 * <p>Namespace prefix separator.</p>
+	 * 
+	 * <p>The default value is <code>':'</code>.</p>
+	 */
 	char namespaceSeparator() default ':';
 }
