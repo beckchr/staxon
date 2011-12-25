0e369837917fafb96bcf0db612123f89f399caff
Christoph Beck <beck@odysseus.de>
2011-12-25 19:35:18 +0100

    added convenience methods accepting Input/OutputStream

--- core/src/main/java/de/odysseus/staxon/json/jaxb/JsonXMLMapper.java
+++ core/src/main/java/de/odysseus/staxon/json/jaxb/JsonXMLMapper.java
@@ -15,8 +15,13 @@
  */
 package de.odysseus.staxon.json.jaxb;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.List;
 
@@ -28,7 +33,7 @@ import javax.xml.stream.XMLStreamException;
  * Read/write instances of JAXB-annotated classes from/to JSON.
  */
 public class JsonXMLMapper<T> {
-	protected static JsonXML getConfig(Class<?> type) throws JAXBException {
+	private static final JsonXML getConfig(Class<?> type) throws JAXBException {
 		@JsonXML
 		class Default {}
 		JsonXML config = type.getAnnotation(JsonXML.class);
@@ -37,6 +42,8 @@ public class JsonXMLMapper<T> {
 		}
 		return config;
 	}
+	
+	private static final Charset UTF_8 = Charset.forName("UTF-8");
 	
 	private final Class<T> type;
 	private final JsonXML config;
@@ -66,15 +73,31 @@ public class JsonXMLMapper<T> {
 		return binder.readObject(type, config, context, reader);
 	}
 
+	public T readObject(InputStream input) throws JAXBException, XMLStreamException {
+		return readObject(new InputStreamReader(input, UTF_8));
+	}
+
 	public void writeObject(Writer writer, T value) throws JAXBException, XMLStreamException {
 		binder.writeObject(type, config, context, writer, value);
 	}
+	
+	public void writeObject(OutputStream output, T value) throws JAXBException, XMLStreamException {
+		writeObject(new OutputStreamWriter(output, UTF_8), value);
+	}	
 
 	public List<T> readArray(Reader reader) throws JAXBException, XMLStreamException {
 		return binder.readArray(type, config, context, reader);
 	}
 
+	public List<T> readArray(InputStream input) throws JAXBException, XMLStreamException {
+		return readArray(new InputStreamReader(input, UTF_8));
+	}
+
 	public void writeArray(Writer writer, Collection<T> collection) throws JAXBException, XMLStreamException {
 		binder.writeArray(type, config, context, writer, collection);
+	}
+
+	public void writeArray(OutputStream output, Collection<T> collection) throws JAXBException, XMLStreamException {
+		writeArray(new OutputStreamWriter(output, UTF_8), collection);
 	}
 }
